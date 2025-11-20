a029c163d356a392a3d3ffd2379d15a150bbaaba
livinusonynso <livinusonyenso@gmail.com>
2025-11-20 19:26:44 +0100

    fix eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,37 @@ import tseslint from 'typescript-eslint'
 
 export default [
   { ignores: ['dist'] },
+
+  // TypeScript + React config
   {
     files: ['**/*.{ts,tsx}'],
+
     languageOptions: {
+      parser: tseslint.parser,
       ecmaVersion: 2020,
       sourceType: 'module',
       globals: globals.browser,
     },
+
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
     rules: {
-      'react-refresh/only-export-components': 'warn',
+      // TypeScript Rules
+      ...tseslint.configs.recommended.rules,
+
+      // React Hooks Rules
+      ...reactHooks.configs.recommended.rules,
+
+      // React Refresh Rule
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // Optional: Good defaults
+      'no-unused-vars': 'off', // turned off in favor of TS version
+      '@typescript-eslint/no-unused-vars': ['warn'],
     },
   },
 ]
